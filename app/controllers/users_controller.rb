463cf369c0de3d7efbaa19bec99988a516a69f94
ndaniele <n.daniele@gmail.com>
2016-08-16 13:22:57 -0400

    Users M: added V and C

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    #raise params.inspect
+    #@user = User.new
+    #@user.email = params[:user][:email]
+    #@user.save
+    
+    @user = User.new(:email => params[:user][:email]) 
+    if 
+      @user.save
+      redirect_to root_path
+    else
+      render 'users/new'
+    end   
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates_presence_of :email #, :message => "you need an email"
+  validates :email, :uniqueness => true #, :message => "need a unique email"
 end
