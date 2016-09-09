8a4228e01a925266eed241f8d6459faab42cac9a
ndaniele <n.daniele@gmail.com>
2016-09-09 15:42:44 -0400

    added update action to groups MVC

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,16 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find_by(:id => params[:id])
   end
+
+  def update
+    #raise params.inspect
+    @user = User.find_by(:id => params[:group][:user_id])
+      #=> returns user "2"
+    @group = Group.find_by(id: params[:id])
+      #=> returns group '3'
+    @user.groups << @group
+    @group.save
+    redirect_to group_path(@group)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ end
 
 devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resources :groups, :only => [:show, :create, :new, :index]
+  resources :groups, :only => [:show, :create, :new, :index, :update]
 
   resources :users, :only => [:show, :create, :new]
 
