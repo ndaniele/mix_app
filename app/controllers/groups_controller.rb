080199a16d4b1a7b8011eed032a52dc256579534
ndaniele <n.daniele@gmail.com>
2016-10-04 15:32:57 -0400

    refactored Group#new so that user who creates group automatically joins it via new membership

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,15 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.memberships.build
+    #@group.memberships.build(:user_id => current_user.id, :group_id => @group.id)
+    #@membership = Membership.new(:user_id => current_user.id, :group_id => @group.id)
+    #@membership.save
   end
 
   def create 
     #raise params.inspect 
     @group = Group.create(group_params)
+    @group.memberships.build(:user_id => params[:group][:user_id], :group_id => @group.id).save
     redirect_to group_path(@group)
   end
 
